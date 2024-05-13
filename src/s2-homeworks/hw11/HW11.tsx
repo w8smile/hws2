73ba077a715ca0cd74e4bceabc38e1d179f4f941
Serg Nakovnick <w8smile@gmail.com>
2024-05-13 18:13:59 +0300

    HW11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -14,10 +14,20 @@ function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
-
     const change = (event: any, value: any) => {
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            setValue1(event.target.value)
+        }
+        console.log(value)
+
+
+
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
+
 
     return (
         <div id={'hw11'}>
@@ -29,6 +39,8 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            onChange={(event: any, value: any) => {change(event, value)}}
+                            value={value1}
                             // сделать так чтоб value1 изменялось // пишет студент
 
                         />
@@ -37,8 +49,9 @@ function HW11() {
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            onChange={(event: any, value: any) => {change(event, value)}}
+                            value={[value1, value2]}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
